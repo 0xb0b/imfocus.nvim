1fa3f1f6a233e3740a21de581d85e3f8c42996f4
Bohdan Budnyy <bohdanxbudnyy@gmail.com>
2019-06-29 22:14:56 +0300

    - added automatic highlight settings calculation - added soft_shadow and lightness settings - refactoring

--- nvim/plugins/wip/init.vim
+++ nvim/plugins/wip/init.vim
@@ -3,8 +3,6 @@ execute "source /home/xbob/config/nvim/init.vim"
 " add current dir to runtime path
 let &runtimepath.=",".escape(expand("<sfile>:p:h"), "\,")
 
-hi InsertShadow ctermfg=238 guifg=#444444
-
-let g:wip_hl_group = "InsertShadow"
 let g:wip_focus_size = 1
+let g:wip_soft_shadow = 1
 
--- nvim/plugins/wip/rplugin/python3/wip.py
+++ nvim/plugins/wip/rplugin/python3/wip.py
@@ -1,16 +1,116 @@
 import pynvim
+from math import sqrt
+
+
+hl_group_normal = "Normal"
+default_hl_group = "wipInsertShadow"
+default_lightness = 0.2
+
+g_focus_size = "wip_focus_size"
+g_hl_group = "wip_hl_group"
+g_lightness = "wip_lightness"
+g_soft_shadow = "wip_soft_shadow"
+
+
+def rgb_channels(color):
+    color = int(color)
+    r = (color >> 16) & 0xff
+    g = (color >> 8) & 0xff
+    b = color & 0xff
+    return [r, g, b]
+
+
+def blend(color_a, color_b, coeff):
+    # blending coefficient 0 results in pure color_a, coefficient 1 results in pure color_b
+    #  return [round(sqrt((1 - coeff) * channel_a**2 + coeff * channel_b**2))
+            #  for channel_a, channel_b in zip(color_a, color_b)]
+    return [round((1 - coeff) * channel_a + coeff * channel_b)
+            for channel_a, channel_b in zip(color_a, color_b)]
+
+
+def rgb_pack(channels):
+    r, g, b = channels
+    color = (((r << 8) | g) << 8) | b
+    return "#{:x}".format(color)
+
 
 @pynvim.plugin
 class Wip:
-
     def __init__(self, nvim_ref):
+        # do not do anything with non-trivial side-effects here! seriously.
         self.nvim = nvim_ref
-        self.hl_group = None
-        self.focus_size = None
         self.hl_src = None
+        # plugin settings
+        self.focus_size = None
+        self.hl_group = None
+        self.lightness = None
+        self.has_soft_shadow = None
+        # state
         self.window = None
         self.cursor_line = None
         self.match_ids = set()
+
+    @pynvim.autocmd("InsertEnter", pattern="*", sync=True)
+    def on_insert_enter(self):
+        if self.focus_size is None:
+            self.init()
+        self.focus()
+
+    @pynvim.autocmd("InsertLeave", pattern="*", sync=True)
+    def on_insert_leave(self):
+        self.window = None
+        self.cursor_line = None
+        self.clear_hl()
+
+    @pynvim.autocmd("CursorMovedI", pattern="*", sync=True)
+    def on_cursor_moved(self):
+        self.focus()
+
+    def init(self):
+        # this function is needed because there should not be any nvim interaction in __init__()
+        self.focus_size = max(0, self.nvim.vars.get(g_focus_size, 0))
+        self.set_hl_group()
+        # hard shadow by default
+        self.has_soft_shadow = self.nvim.vars.get(g_soft_shadow, 0)
+        self.hl_src = self.nvim.new_highlight_source()
+
+    def set_hl_group(self):
+        self.hl_group = self.nvim.vars.get(g_hl_group, None)
+        if self.hl_group is None:
+            self.hl_group = default_hl_group
+        if not self.nvim.funcs.hlexists(self.hl_group):
+            self.highlight()
+
+    def highlight(self):
+        # get Normal foreground color and blend into background
+        rgb_hl = self.get_option("gui_running", False) or self.get_option("termguicolors", False)
+        term_hl = (self.get_option("t_Co") == 256)
+        if not rgb_hl and not term_hl:
+            self.nvim.err_write("<plugin_name> is disabled, only rgb or 256 colors are supported\n")
+            # TODO disable plugin
+            return
+
+        normal_hl_map = self.nvim.api.get_hl_by_name(hl_group_normal, rgb_hl)
+        fg = normal_hl_map.get("foreground")
+        bg = normal_hl_map.get("background")
+        if fg is None or bg is None:
+            self.nvim.err_write("<plugin_name> is disabled, Normal colors undefined\n")
+            # TODO disable plugin
+            return
+
+        # blend shadow color
+        self.lightness = self.nvim.vars.get(g_lightness, default_lightness)
+        if rgb_hl:
+            shadow_color = rgb_pack(blend(rgb_channels(bg), rgb_channels(fg), self.lightness))
+        else:
+            # TODO cterm case, steal from junegunn/limelight.vim
+            shadow_color = "#444444"
+        self.nvim.funcs.execute("hi {} guifg={}".format(self.hl_group, shadow_color))
+
+    def clear_hl(self):
+        for match_id in self.match_ids:
+            self.nvim.funcs.matchdelete(match_id)
+        self.match_ids.clear()
 
     def focus(self):
         # on_insert_enter/leave are asynchronous handlers
@@ -23,7 +123,6 @@ class Wip:
         if self.window is None:
             self.window = self.nvim.current.window
 
-        # lines are 1-indexed
         cursor_line = self.window.cursor[0]
         if self.cursor_line != cursor_line:
             self.cursor_line = cursor_line
@@ -38,10 +137,7 @@ class Wip:
             # last line in focus
             focus_end = min(bottom_line, cursor_line + self.focus_size)
 
-            for match_id in self.match_ids:
-                self.nvim.funcs.matchdelete(match_id)
-            self.match_ids.clear()
-
+            self.clear_hl()
             for line in range(top_line, focus_start):
                 match_id = self.nvim.funcs.matchaddpos(self.hl_group, [line])
                 self.match_ids.add(match_id)
@@ -49,28 +145,10 @@ class Wip:
                 match_id = self.nvim.funcs.matchaddpos(self.hl_group, [line])
                 self.match_ids.add(match_id)
 
-    @pynvim.autocmd("InsertEnter", pattern="*", sync=True)
-    def on_insert_enter(self):
-        if self.hl_group is None:
-            self.hl_group = self.nvim.vars.get("wip_hl_group", None)
-        if self.hl_group is None:
-            self.nvim.out_write("#dbg hl_group is None\n")
-        else:
-            if self.hl_src is None:
-                self.hl_src = self.nvim.new_highlight_source()
-            if self.focus_size is None:
-                self.focus_size = max(0, self.nvim.vars.get("wip_focus_size", 0))
-            self.focus()
-
-    @pynvim.autocmd("InsertLeave", pattern="*", sync=True)
-    def on_insert_leave(self):
-        self.window = None
-        self.cursor_line = None
-        for match_id in self.match_ids:
-            self.nvim.funcs.matchdelete(match_id)
-        self.match_ids.clear()
-
-    @pynvim.autocmd("CursorMovedI", pattern="*", sync=True)
-    def on_cursor_moved(self):
-        self.focus()
+    def get_option(self, name, default=None):
+        try:
+            option = self.nvim.api.get_option(name)
+        except pynvim.api.nvim.NvimError:
+            option = default
+        return option
 
